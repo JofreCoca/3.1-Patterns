28ce299296c3323e913786d3d31e0bdf61e22e76
JofreCoca <jofrecoca@gmail.com>
2025-01-20 13:09:11 +0100

    Create Undo

--- src/Undo.java
+++ src/Undo.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Undo {
+    private static Undo instanceUndo;
+    private List<String> commands;
+    private Undo(){
+        this.commands=new ArrayList<String>();
+    }
+    public static Undo getInstanceUndo(){
+        if(instanceUndo==null){
+            instanceUndo=new Undo();
+        }
+        return instanceUndo;
+    }
+    public void addCommand(String command){
+        this.commands.add(command);
+        System.out.println("Command introduced");
+    }
+    public void deleteCommand(int position){
+        this.commands.remove(position);
+        System.out.println("Command removed");
+    }
+    public void listCommands(){
+        commands.forEach(System.out::println);
+    }
 }
